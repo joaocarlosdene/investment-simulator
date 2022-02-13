223296d53d76950e01b790715fd64601c301b906
Joao Carlos Dene <joaoo.c@hotmail.com>
2022-02-12 21:25:58 -0300

    configurando react dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 import Resultado from './pages/resultado'
 import Simulador from'./components/simulador'
 function App() {
   return (
+    
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/resultado" element={<Resultado/>}></Route>
+        </Routes>
+      </Router>
+      
       <h1>Simulador de Investimentos</h1>
       <Simulador/>
     </div>
--- frontend/src/components/simulador.jsx
+++ frontend/src/components/simulador.jsx
@@ -5,10 +5,10 @@ import { Col, Container, Row } from "react-bootstrap";
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+
 
 import Resultado from '../pages/resultado'
-
 
 
 export default props =>
@@ -45,28 +45,32 @@ export default props =>
                 </Button>
             </Col>
             <Col className="col-12 col-md-4 text-center service-box2 ">
-            <p>Tipos de indexação</p>
-            <ButtonGroup size="lg" className="mb-2">
-                <Button>PRÉ-FIXADO</Button>
-                <Button>POS-FIXADO</Button>
-            </ButtonGroup>
-            <Form>
-                <Form.Group className="mb-3" controlId="formGroup">
-                    <Form.Label>Aporte Mensal</Form.Label>
-                    <Form.Control type="number" placeholder="Digite aqui seu aporte" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formGroup">
-                    <Form.Label>Rentabilidade</Form.Label>
-                    <Form.Control type="number" placeholder="Digite aqui o prazo" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formGroup">
-                    <Form.Label>CDI (ao ano)</Form.Label>
-                    <p>9,18%</p>
-                </Form.Group>
-            </Form>
-                        <Button variant="primary" type="submit">
+                <p>Tipos de indexação</p>
+                <ButtonGroup size="lg" className="mb-2">
+                    <Button>PRÉ-FIXADO</Button>
+                    <Button>POS-FIXADO</Button>
+                </ButtonGroup>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formGroup">
+                        <Form.Label>Aporte Mensal</Form.Label>
+                        <Form.Control type="number" placeholder="Digite aqui seu aporte" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroup">
+                        <Form.Label>Rentabilidade</Form.Label>
+                        <Form.Control type="number" placeholder="Digite aqui o prazo" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroup">
+                        <Form.Label>CDI (ao ano)</Form.Label>
+                        <p>9,18%</p>
+                    </Form.Group>
+                </Form>
+                
+                
+                    <Button variant="primary" type="submit">
                             Simular
-                        </Button>
+                    </Button>
+                    
+                        
             </Col>
         </Row>
         </Container>
